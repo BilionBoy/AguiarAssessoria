2db5b91d3b23d184927ca29c854e6a8d2013fcc3
Dhiogoandre <dhiogoandre26@gmail.com>
2025-09-03 23:55:51 -0400

    locales: pt-br clients attributes andm odel

--- app/models/e_cliente.rb
+++ app/models/e_cliente.rb
@@ -3,4 +3,5 @@
 class ECliente < ApplicationRecord
   # Adicione aqui quaisquer métodos ou validações padrão para seus modelos
   belongs_to :e_empresa
+  belongs_to :g_status_cliente, optional: true
 end
