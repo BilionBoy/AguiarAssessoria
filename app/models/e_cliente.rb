d93e977fca806c3d299cf2ed5f8e0ef1481c129d
BilionBoy <dhiogoandre26@gmail.com>
2025-09-04 19:22:06 -0400

    wip: relacionamentos cliente

--- app/models/e_cliente.rb
+++ app/models/e_cliente.rb
@@ -4,4 +4,12 @@ class ECliente < ApplicationRecord
   # Adicione aqui quaisquer métodos ou validações padrão para seus modelos
   belongs_to :e_empresa
   belongs_to :g_status_cliente, optional: true
+  belongs_to :g_sexo
+  belongs_to :g_estado
+  belongs_to :g_cidade
+  belongs_to :g_bairro
+  belongs_to :g_orgao
+  belongs_to :g_margem_tipo
+  belongs_to :g_tipo_beneficio
+  belongs_to :g_margem_tipo
 end
