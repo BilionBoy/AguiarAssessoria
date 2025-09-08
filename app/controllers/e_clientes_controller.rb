0989c9b23c659d5f874ca722aaa1533ef66c3a84
Dhiogoandre <dhiogoandre26@gmail.com>
2025-09-08 18:23:38 -0400

    validacoes clientes a empresa

--- app/controllers/e_clientes_controller.rb
+++ app/controllers/e_clientes_controller.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 class EClientesController < ApplicationController
   before_action :set_e_cliente, only: %i[show edit update destroy]
 
@@ -17,7 +18,7 @@ class EClientesController < ApplicationController
   end
 
   def create
-    @e_cliente = ECliente.new(e_cliente_params)
+    @e_cliente = current_user.e_empresa.e_clientes.new(e_cliente_params)
 
     if @e_cliente.save
       redirect_to e_clientes_path, notice: t('messages.created_successfully')
@@ -39,18 +40,20 @@ class EClientesController < ApplicationController
       redirect_to e_clientes_url, notice: t('messages.deleted_successfully')
     else
       redirect_to e_clientes_url, alert: t('messages.delete_failed_due_to_dependencies')
-    end   
+    end
   end
 
   private
 
   def set_e_cliente
     @e_cliente = ECliente.find_by(id: params[:id])
-    return redirect_to e_clientes_path, alert: t('messages.not_found') unless @e_cliente
+    redirect_to e_clientes_path, alert: t('messages.not_found') unless @e_cliente
   end
 
   def e_cliente_params
-    permitted_attributes = ECliente.column_names.reject { |col| ['deleted_at', 'created_by', 'updated_by'].include?(col) }
+    permitted_attributes = ECliente.column_names.reject do |col|
+      %w[deleted_at created_by updated_by].include?(col)
+    end
     params.require(:e_cliente).permit(permitted_attributes.map(&:to_sym))
   end
 
