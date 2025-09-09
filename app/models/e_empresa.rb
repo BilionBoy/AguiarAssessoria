36b0d1aab1898a8317d520430019092f6b8dfaed
Dhiogoandre <dhiogoandre26@gmail.com>
2025-09-08 23:25:54 -0400

    index de clientes

--- app/models/e_empresa.rb
+++ app/models/e_empresa.rb
@@ -3,7 +3,7 @@
 class EEmpresa < ApplicationRecord
   # Adicione aqui quaisquer métodos ou validações padrão para seus modelos
   belongs_to :g_cidade
-  has_many :e_clientes, dependent: :nullify
+  has_many   :e_clientes, dependent: :nullify
 
   validates :nome_fantasia, presence: true
   validates :razao_social,  presence: true
