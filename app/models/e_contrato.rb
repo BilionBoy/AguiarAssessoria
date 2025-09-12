c55b178db9cd9ebcd4ce7e886145d2f3c194a5bb
root <root@DESKTOP-2JC6FVQ.localdomain>
2025-09-12 19:18:37 -0400

    initial validacoes contratos

--- app/models/e_contrato.rb
+++ app/models/e_contrato.rb
@@ -2,5 +2,10 @@
 
 class EContrato < ApplicationRecord
   # Adicione aqui quaisquer métodos ou validações padrão para seus modelos
-  
+  validates :quantidade_parcelas, presence: true
+  validates :numero_contrato,     presence: true
+  validates :valor_total,         presence: true
+  validates :valor_comissao,      presence: true
+  validates :data_inicio,         presence: true
+  validates :data_fim,            presence: true
 end
